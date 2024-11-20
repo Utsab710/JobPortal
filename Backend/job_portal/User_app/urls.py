1f5fa7658873f940de5bd1244951820bcd83d319
hatsimitsu <onusha45@gmail.com>
2024-11-20 08:20:56 +0545

    user table updated

---
+++ Backend/job_portal/User_app/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from User_app.views import *
+from .user_viewset import UsersViewsets
+
+
+router = DefaultRouter()
+router.register('users',UsersViewsets,basename='UsersViewsets')
+
+
+urlpatterns =   [
+     path('signup/', signup ,name='signup'),
+     path('employer_signup', employer_signup, name="employer_signup"),
+     path('jobseeker_signup', jobseeker_signup, name="jobseeker_signup"),
+     path('login/',login,name='login'),
+     path('jobseekerhome/', jobseekerhome ,name='jobseekerhome'),
+     path('employerhome/', employerhome ,name='employerhome'),
+     path('users/',include(router.urls)),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
