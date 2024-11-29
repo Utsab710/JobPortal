18591afabd5b10cce20f99b08f49901807ab0c7c
hatsimitsu <onusha45@gmail.com>
2024-11-29 13:18:51 +0545

    edited

--- Backend/jobportal_backend/seeker/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SeekerSignup
-# Register your models here.
-
-admin.site.register(SeekerSignup)
--- Backend/jobportal_backend/seeker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SeekerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seeker'
